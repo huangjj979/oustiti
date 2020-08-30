5fc759f6c81318179723e84b4650ad50ef979d8a
方泓睿 <chfanghr@gmail.com>
2020-08-30 20:48:33 +0800

    build: directly push to dockerhub

--- tools/build.sh
+++ tools/build.sh
@@ -3,5 +3,5 @@
 export CGO_ENABLED=0
 
 go build -tags netgo -v -o artifacts/outstiti .
-docker build -t outstiti -f tools/Dockerfile .
-docker save -o artifacts/outstiti_docker.tar outstiti
+#docker build -t outstiti -f tools/Dockerfile .
+#docker save -o artifacts/outstiti_docker.tar outstiti
